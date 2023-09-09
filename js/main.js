0d95c772fbfc7c8ddaac8bfcfb06326fd53e6646
root <mohammad.pr285@gmail.com>
2023-09-09 17:03:00 -0500

    super offer section complited

--- js/main.js
+++ js/main.js
@@ -18,3 +18,19 @@ setInterval(()=>{
     }
     slideSet(`slide${slideIndex}`, slideIndex);
 }, 5000);
+
+// offer countdown 
+
+var countDownDate = new Date().getTime() + (24 * 60 * 60 * 1000 );
+
+var x = setInterval(function() {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+})
